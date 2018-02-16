57dfd24591501eac1b1d64b246e3f6499be09cdd
noud02 <noud.kerver@hotmail.nl>
2018-02-16 15:24:57 +0100

    add codacy plugin

---
+++ src/plugins/CodacyPlugin.js
@@ -0,0 +1,33 @@
+const BasePlugin = require('../BasePlugin');
+
+class Codacy extends BasePlugin {
+    static generateEmbed(body) {
+        let color = 0xFFFFFF;
+        let status = 'Unknown';
+
+        if (body.commit.results.new_count > 0) {
+            color = 0xF44336;
+            status = 'New errors';
+        } else {
+            color = 0x4CAF50;
+            status = 'No errors';
+        }
+
+        return {
+            title: status,
+            color,
+            url: body.commit.data.urls.delta,
+            description: `\`${body.commit.data.uuid.substring(0, 7)}\` Fixed: ${body.commit.results.fixed_count} New: ${body.commit.results.new_count}`
+        };
+    }
+
+    static execute(body) {
+        return {
+            service: 'Codacy',
+            logo: 'https://p14.zdassets.com/hc/settings_assets/960438/200158055/j2MxqjmX2ar8LeC06jBR8g-S_B_Big.png',
+            embed: Codacy.generateEmbed(body)
+        };
+    }
+}
+
+module.exports = Codacy;
