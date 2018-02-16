b0a6a8dc6278d96169d9196e11f95e60aca9a069
noud02 <noud.kerver@hotmail.nl>
2018-02-16 14:47:13 +0100

    fix travis plugin

--- src/plugins/TravisPlugin.js
+++ src/plugins/TravisPlugin.js
@@ -2,18 +2,20 @@ const BasePlugin = require('../BasePlugin');
 
 class Travis extends BasePlugin {
     static execute(body) {
+        const payload = JSON.parse(body.payload);
+
         return {
             service: 'Travis CI',
             logo: 'https://travis-ci.org/images/logos/TravisCI-Mascot-1.png',
             embed: BasePlugin.generateEmbed({
-                commitId: body.payload.commit,
-                commitMessage: body.payload.message,
-                commitAuthor: body.payload.author_name,
-                status: body.payload.state,
-                url: body.payload.build_url,
-                repository: body.payload.repository.name,
-                repositoryOwner: body.payload.repository.owner_name,
-                branch: body.payload.branch
+                commitId: payload.commit,
+                commitMessage: payload.message,
+                commitAuthor: payload.author_name,
+                status: payload.state,
+                url: payload.build_url,
+                repository: payload.repository.name,
+                repositoryOwner: payload.repository.owner_name,
+                branch: payload.branch
             })
         };
     }
